de09fa0849498f777cce956c54a32583b1df9884
ErivanDev <gasparzinho633@gmail.com>
2024-08-01 22:15:16 -0300

    novos caminhos

--- ServidorREST/main.py
+++ ServidorREST/main.py
@@ -37,15 +37,17 @@ def home():
     return render_template('./index.html')
 
 @app.route('/search', methods=['GET'])
-def search_book(title):
+def search_book():
     # Base URL for Open Library search API
     search_url = "https://openlibrary.org/search.json"
 
-    title = request.json.get('title')
+    title = request.args.get('title')
+    limit = request.args.get('limit')
 
     # Parameters for the search query
     params = {
-        'title': title
+        'title': title,
+        'limit': limit
     }
 
     # Send GET request to Open Library API
@@ -55,16 +57,12 @@ def search_book(title):
     if response.status_code == 200:
         # Parse JSON response
         data = response.json()
-        # Print the search results
-        for book in data['docs']:
-            print(f"Title: {book.get('title')}")
-            print(f"Author: {book.get('author_name', ['Unknown'])[0]}")
-            print(f"First Published Year: {book.get('first_publish_year')}")
-            print()
+        return data, 200
     else:
-        print(f"Error: {response.status_code}")
+        return jsonify({'error': 'Books not found'}), 404
 
 # Create a new book
+'''
 @app.route('/books', methods=['POST'])
 def add_book():
     isbn = request.json.get('isbn')
@@ -75,7 +73,10 @@ def add_book():
     if not book_data:
         return jsonify({'error': 'Book not found'}), 404
     
+    book_id_counter = 1 if len(data['books']) == 0 else data['books'][len(data['books'])-1]['id'] + 1
+    
     book = {
+        'id': book_id_counter,
         'isbn': isbn,
         'title': book_data.get('title'),
         'authors': [author['name'] for author in book_data.get('authors', [])],
@@ -85,6 +86,7 @@ def add_book():
     books.append(book)
     save_data({'books': books})
     return jsonify(book), 201
+'''
 
 # Get all books
 @app.route('/books', methods=['GET'])
@@ -96,9 +98,28 @@ def get_books():
 def add_review(isbn):
     book = next((b for b in books if b['isbn'] == isbn), None)
     if not book:
-        return jsonify({'error': 'Book not found'}), 404
+        # return jsonify({'error': 'Book not found'}), 404
+        book_data = fetch_book_data(isbn)
+        if not book_data:
+            return jsonify({'error': 'Book not found'}), 404
+        
+        book_id_counter = 1 if len(data['books']) == 0 else data['books'][len(data['books'])-1]['id'] + 1
     
+        book = {
+            'id': book_id_counter,
+            'isbn': isbn,
+            'title': book_data.get('title'),
+            'authors': [author['name'] for author in book_data.get('authors', [])],
+            'cover': book_data.get('cover', {}).get('large'),
+            'reviews': []
+        }
+        books.append(book)
+        save_data({'books': books})
+    
+    review_id_counter = 1 if len(book['reviews']) == 0 else book['reviews'][len(book['reviews'])-1]['id'] + 1
+
     review = {
+        'id': review_id_counter,
         'reviewer': request.json.get('reviewer'),
         'text': request.json.get('text'),
         'rating': request.json.get('rating')
@@ -106,6 +127,47 @@ def add_review(isbn):
     book['reviews'].append(review)
     save_data({'books': books})
     return jsonify(review), 201
+
+# Update a review to a book
+@app.route('/books/<isbn>/reviews/<int:id>', methods=['PUT'])
+def update_review(isbn, id):
+    book = next((b for b in books if b['isbn'] == isbn), None)
+    if not book:
+        return jsonify({'error': 'Book not found'}), 404
+    
+    review = next((r for r in book['reviews'] if r['id'] == id), None)
+    if not review:
+        return jsonify({'error': 'Review not found'}), 404
+
+    review_data = request.json
+    if 'reviewer' in review_data:
+        review['reviewer'] = review_data['reviewer']
+    if 'text' in review_data:
+        review['text'] = review_data['text']
+    if 'rating' in review_data:
+        review['rating'] = review_data['rating']
+    
+    save_data({'books': books})
+    return jsonify(review), 200
+
+# Delete a review to a book
+@app.route('/books/<isbn>/reviews/<int:id>', methods=['DELETE'])
+def delete_review(isbn, id):
+    book = next((b for b in books if b['isbn'] == isbn), None)
+    if not book:
+        return jsonify({'error': 'Book not found'}), 404
+    
+    reviews = [r for r in book['reviews'] if r['id'] != id]
+    if len(reviews) == len(book['reviews']):
+        return jsonify({'error': 'Review not found'}), 404
+
+    for b in books:
+        if b['isbn'] == isbn:
+            b['reviews'] = reviews
+            break
+    
+    save_data({'books': books})
+    return jsonify(reviews), 200
 
 # Get all reviews for a book
 @app.route('/books/<isbn>/reviews', methods=['GET'])
